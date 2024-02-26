920e17c34595a220cb7b6077d58870376ac2fdb1
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-02-26 11:34:00 +0100

    Fix catalogue component

--- src/components/Catalogue/Catalogue.jsx
+++ src/components/Catalogue/Catalogue.jsx
@@ -1,17 +1,28 @@
 // https://www.youtube.com/watch?v=B9tDYAZZxcE
 // me quedé por minuto 13
+import React, { useState, useEffect } from 'react';
+import ProductHandler from "../../handler/ProductHandler";
 
 function Catalogue() {
-    const [filters, setFilters] = useState({
-        category: "all",
-        minPrice: 0
-    });
+    const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+        ProductHandler.getProducts().then(products => {
+            setProducts(products);
+        });
+    }, []); // Pasar un array vacío como segundo argumento para que el efecto solo se ejecute una vez al montar el componente
 
     return (
-        <h1>Catalogue</h1>
-    
-    )
+        <>
+            {products.map(product => (
+                <div key={product.id}>
+                    <h1>{product.productName}</h1>
+                    <img src={product.productImage} alt={product.productName} />
+                    <p>{product.productDescription}</p>
+                </div>
+            ))}
+        </>
+    );
 }
 
 export default Catalogue;

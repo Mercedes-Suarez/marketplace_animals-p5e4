63abb5fb40f4ae0f294505c5921ab9f69e54b263
claudia <claumarlopez1978@gmail.com>
2024-02-27 15:13:34 +0100

    changes router

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,40 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import ProductHandler from "../../handler/ProductHandler";
 import './card.css'
+import { Link } from 'react-router-dom';
 
 function Card() {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        ProductHandler.getProducts().then(products => {
-            setProducts(products);
-        });
-    }, []);
-    return (
-        <div className="product-container">
-          {products.map((product, index) => (
-            <div key={index} className="card">
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    ProductHandler.getProducts().then(products => {
+      setProducts(products);
+    });
+  }, []);
+  return (
+    <div className="product-container">
+      {products.map((product, index) => (
+        <div key={index} className="card">
+          <Link to={{ pathname: `/product/${product.id}`, state: { product } }}>
+            <div className='image-container'>
               <img
                 src={product.productImage}
                 alt={product.productName}
               />
-              <div className="product-details">
-                <h3 className="product-title">{product.productName}</h3>
-            <p className="product-description">{product.productDescription}</p>
-                <div className="price-details">
-                  <span className="price">{product.productPrice.monto} {product.productPrice.moneda}</span>
-                  <button className="like-button">
-                    <img src="/images/heart-icon.svg" alt="Me gusta" id="heart-icon" />
-                  </button>
-                </div>
-              </div>
-              <div className="add-to-cart">
-                <button className="add-to-cart-button">
-                  añadir al carrito
-                </button>
+              <div class="overlay">
+                Ver más
               </div>
             </div>
-          ))}
+          </Link>
+          <div className="product-details">
+            <h3 className="product-title">{product.productName}</h3>
+            <p className="product-description">{product.productDescription}</p>
+            <div className="price-details">
+              <span className="price">{product.productPrice.monto} {product.productPrice.moneda}</span>
+              <button className="like-button">
+                <img src="/images/heart-icon.svg" alt="Me gusta" id="heart-icon" />
+              </button>
+            </div>
+          </div>
+          <div className="add-to-cart">
+            <button className="add-to-cart-button">
+              añadir al carrito
+            </button>
+          </div>
         </div>
-      );
+      ))}
+    </div>
+  );
 }
 export default Card;

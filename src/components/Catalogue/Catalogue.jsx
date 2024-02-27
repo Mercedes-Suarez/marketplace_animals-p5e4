16dde4a56566da8137c43ffa9783c6e2f5bf120f
Clara Ruiz-Santacruz Jiménez <clara.ruizsantacruz@gmail.com>
2024-02-27 13:55:40 +0100

    add styles to catalogue

--- src/components/Catalogue/Catalogue.jsx
+++ src/components/Catalogue/Catalogue.jsx
@@ -1,25 +1,33 @@
 import './catalogue.css'
 import Card from '../Card/Card';
 
+
 function Catalogue() {
 
     return (
-        <div>
+        <div className="catalogue-body">
 
             <section>
-                <img/>
+            <img className="header-catalogue" src="/public/images/HeaderCatalogue.png" alt="HeaderCatalogueView" />
             </section>
 
-            <section>
-                <h2>nuestros productos</h2>
-                <hr />
-                <button>gato</button>
-                <button>perro</button>
-                <hr />
-                <button>juguetes</button>
-                <button>chuches</button>
-                <button>camas</button>
-                <button>correas</button>
+            <section className='filters-container'> 
+                <h2 id="filters-title" >nuestros productos</h2>
+                <hr className="filters-lines"/>
+
+                <div className='categories-buttons-container'>
+                <button className="categories-buttons">perro</button>
+                <button className="categories-buttons">gato</button>
+                </div>
+
+                <hr className="filters-lines"/>
+
+                <div className='subcategories-buttons-container'>
+                <button  className="subcategories-buttons">juguetes</button>
+                <button  className="subcategories-buttons">chuches</button>
+                <button  className="subcategories-buttons">correas</button>
+                <button  className="subcategories-buttons">camas</button>
+                </div>
             </section>
 
             <section>

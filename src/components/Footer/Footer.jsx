1a23a20765fd895fe21d714c07405387a18b68ce
inesgallo <anainesgf@gmail.com>
2024-02-27 13:26:58 +0100

    added home

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer>
+      <section className="footerbrown">
+        <div>
+        <img src="public\images\logoclaro.svg" alt="logo" />
+          <div>
+            <p>politica de privacidad</p>
+            <p>aviso legal</p>
+          </div>
+        </div>
+      </section>
+      <section className="footerred">
+        <h4>desarrollado por Grupo 20 Creations</h4>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,65 @@
+<<<<<<< Updated upstream
+=======
+// Desk_Home_General (Home)
+
+// Componente Navbar (con renderizado condicional)
+// Trozo decorativo de fotos y texto
+// Componente Catálogo
+// Componente Footer
+// Component Login (MODAL)
+
+import Navbar from "../../components/Navbar/Navbar";
+import Catalogue from "../../components/Catalogue/Catalogue";
+import Footer from "../../components/Footer/Footer";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
+
+function Home() {
+  const navigate = useNavigate();
+
+  const goToProducts = () => {
+    navigate("/home/products");
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="bodyhome">
+        <section className="homeHeader">
+          <img src="public\images\portadaHome.png" alt="Portada" />
+        </section>
+        <section className="homeServices">
+          <img src="public\images\imgServices.png" alt="Servicios" />
+          <article className="infoServices">
+            <h2 className="title2Home">nuestra filosofía</h2>
+            <p className="textbody">
+              Seded do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam.
+            </p>
+          </article>
+        </section>
+        <section className="homeProducts">
+          <article className="infoProducts">
+            <h2 className="title2Home">nuestros productos</h2>
+            <p className="textbody">
+              Seded do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div className="buttonbox">
+              <span onClick={goToProducts}>cuéntame más</span>
+            </div>
+          </article>
+          <img src="public\images\imgProducts.png" alt="Productos" />
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
+>>>>>>> Stashed changes

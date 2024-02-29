703bdc364a881420e406f54dafcfe12b614a53fb
elisa.cl <elisacapellinlopez@gmail.com>
2024-02-29 09:55:53 +0100

    formulary update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
 import { useForm, Controller } from "react-hook-form";
 import ProductHandler from "../../handler/ProductHandler";
+import "./form.css";
 
 function Form() {
   const { register, handleSubmit: pHandleSubmit, formState: { errors }, watch, control, reset } = useForm();
@@ -17,79 +18,92 @@ function Form() {
     - setValue: Propiedad/función que permite establecer el valor de un input.
     */
 
-    const onSubmit =
-      pHandleSubmit((data) => { 
-        ProductHandler.submitProduct(data);
-        reset(); // Llamada directa a la función
+  const onSubmit =
+    pHandleSubmit((data) => {
+      ProductHandler.submitProduct(data);
+      reset(); // Llamada directa a la función
     });
 
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="productName">Product name:</label>
-      <input
-        type="text"
-        id="productName"
-        {...register("productName", {
-          required: {
-            value: true,
-            message: "Name is required, please.",
-          },
-          minLength: {
-            value: 2,
-            message: "Name must be at least 2 characters.",
-          },
-        })}
-      />
-      {errors.productName && <span>{errors.productName.message}</span>}
+    <form className="form-container" onSubmit={onSubmit}>
+      <h1>añade un nuevo producto</h1>
 
-      <label htmlFor="productDescription">Description:</label>
-      <input
-        type="text"
-        id="productDescription"
-        {...register("productDescription", {
-          required: {
-            value: true,
-            message: "E-mail is required, please.",
-          },
-        })}
-      />
-      {errors.productDescription && (
-        <span>{errors.productDescription.message}</span>
-      )}
+      <div className="form-input">
+        <label htmlFor="productName"></label>
+        <input
+          type="text"
+          id="productName"
+          {...register("productName", {
+            required: {
+              value: true,
+              message: "Name is required, please.",
+            },
+            minLength: {
+              value: 2,
+              message: "Name must be at least 2 characters.",
+            },
+          })}
+        />
+        {errors.productName && <span>{errors.productName.message}</span>}
+      </div>
 
-      <label htmlFor="productPrice">Price:</label>
-      <input
-        type="number"
-        id="productPrice"
-        {...register("productPrice", {
-          required: {
-            value: true,
-            message: "Price is required, please.",
-          },
-        })}
-      />
-      {errors.productPrice && <span>{errors.productPrice.message}</span>}
+      <div className="form-input">
+        <label htmlFor="productDescription"></label>
+        <input
+          type="text"
+          id="productDescription"
+          {...register("productDescription", {
+            required: {
+              value: true,
+              message: "E-mail is required, please.",
+            },
+          })}
+        />
+        {errors.productDescription && (
+          <span>{errors.productDescription.message}</span>
+        )}
+      </div>
 
-      <label htmlFor="productStock">Stock:</label>
-      <input
-        type="number"
-        id="productStock"
-        {...register("productStock", {
-          required: {
-            value: true,
-            message: "Stock is required, please.",
-          },
-          validate: (value) => { return value ===  0 ? "Stock must be equal to  0." : true; }
-        })}
-      />
-      {errors.productStock && <span>{errors.productStock.message}</span>}
+      <div className="input-container">
+        <div className="form-input">
+          <label htmlFor="productPrice"></label>
+          <input
+            type="number"
+            id="productPrice"
+            {...register("productPrice", {
+              required: {
+                value: true,
+                message: "Price is required, please.",
+              },
+            })}
+          />
+          {errors.productPrice && <span>{errors.productPrice.message}</span>}
+        </div>
 
+        <div className="form-input">
+          <label htmlFor="productStock"></label>
+          <input
+            type="number"
+            id="productStock"
+            {...register("productStock", {
+              required: {
+                value: true,
+                message: "Stock is required, please.",
+              },
+              validate: (value) => { return value === 0 ? "Stock must be equal to  0." : true; }
+            })}
+          />
+          {errors.productStock && <span>{errors.productStock.message}</span>}
+        </div>
+      </div>
+
+      <div className="form-categories">
       <Controller
         name="productCategory"
         control={control}
         defaultValue=""
         render={({ field }) => (
-          <div>
+          <div className="form-cat1">
             cat1:
             <label>
               <input
@@ -100,7 +114,7 @@ function Form() {
               />
               Gato
             </label>
-            
+
             <label>
               <input
                 type="radio"
@@ -114,12 +128,13 @@ function Form() {
         )}
       />
 
+
       <Controller
         name="productSubcategory"
         control={control}
         defaultValue=""
         render={({ field }) => (
-          <div>
+          <div className="form-cat2">
             cat2:
             <label>
               <input
@@ -160,29 +175,31 @@ function Form() {
               />
               Correas
             </label>
-            <br></br>
-            <label htmlFor="productImage">Image:</label>
-            <input
-            type="text"
-                id="productImage"
-                {...register("productImage", {
-                required: {
-                    value: true,
-                    message: "Image is required, please.",
-                },
-                validate: (value) => { return value ===  0 ? "Stock must be equal to  0." : true; }
-                })}
-            />
-            {errors.productImage && <span>{errors.productImage.message}</span>}
           </div>
         )}
       />
+      </div>
+
+      <div className="form-input">
+        <label htmlFor="productImage"></label>
+        <input
+          type="text"
+          id="productImage"
+          {...register("productImage", {
+            required: {
+              value: true,
+              message: "Image is required, please.",
+            },
+            validate: (value) => { return value === 0 ? "Stock must be equal to  0." : true; }
+          })}
+        />
+        {errors.productImage && <span>{errors.productImage.message}</span>}
+      </div>
 
       {/* acá falta el input foto con Cloudinary */}
 
-      <button type="submit">añadir producto</button>
+      <button className="form-button" type="submit">añadir producto</button>
 
-      <pre>{JSON.stringify(watch(), null, 2)}</pre>
     </form>
   );
 }

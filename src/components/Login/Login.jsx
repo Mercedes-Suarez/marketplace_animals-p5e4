8a62aae0d4004c0a2c8f7162748a0ca0c24755ff
Mercedes <jmercedes.suarez@gmail.com>
2024-03-04 09:14:05 +0100

    update open modal

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,85 +1,108 @@
-// Login.jsx
 import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
+import UserHandler from '../../handler/UserHandler';
+import './login.css';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar la visibilidad del modal
   const navigate = useNavigate();
 
-  // const history = useHistory();
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
+    if (!email || !password) {
+      setError('Por favor, rellena todos los campos.');
+      return;
+    }
+
     try {
-      // Reemplaza 'YOUR_API_ENDPOINT' con la URL de tu endpoint de autenticación
-      const response = fetch('http://localhost:5173/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
 
-      if (!response.ok) {
-        const errorData = response.json(); 
-       throw new Error(errorData.message || 'Error al iniciar sesión');
-     
+      let allUsers = await UserHandler.getUsers();
+      const user = allUsers.find(user => user.email === email && user.userPassword === password);
+      if (!user) {
+        throw new Error('Correo electrónico o contraseña incorrectos.');
       }
-         Swal.fire({
-          icon: 'success',
-          title: 'Éxito!',
-          text: 'Sesion iniciada te llevamos a tu perfil',
-        }).then(() => {
-      navigate('/myProducts'); // Redirecciona al usuario a la página de sus productos
-      
-  });
 
-} catch (error) {
-  setError(error.message);
-  Swal.fire({
-    icon: 'error',
-    title: 'Error!',
-    text:error.message,
-  });
-}
-};
-return (
-  <div>
-    <h1>Login</h1>
-    <div className="modal">
-      <div className="modal-content">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Correo electrónico:
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Contraseña:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-          {error && <p>{error}</p>}
-          <button type="submit">Iniciar sesión</button>
-          <button type="button" onClick={() => navigate('/register')}>Registrarse</button>
-        </form>
-        <button onClick={() => navigate('/')}>Volver</button>
-      </div>
+      navigate('/MyProducts');
+
+    } catch (error) {
+      setError(error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: error.message,
+      });
+    }
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>Comienza tu sesión</button> {/* Botón para abrir el modal */}
+
+      {isModalOpen && (
+
+        <div className="modal">
+          <div className="modal-content">
+            <img src="/images/logoHuella.png" alt="Logo" className="logo" />
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label>
+                  Correo electrónico:
+                  <input
+                    type="email"
+                    className="input-email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </label>
+              </div>
+              <div className="form-group">
+                <label>
+                  Contraseña:
+                  <input
+                    type="password"
+                    className="input-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </label>
+              </div>
+              {error && <p>{error}</p>}
+              <div className="button-container">
+                <button type="submit" className="button-login">Iniciar sesión</button>
+                <button type="button" onClick={() => navigate('/register')} className="button-register">Registrarse</button>
+              </div>
+            </form>
+            <div className="image-container">
+            <div className="image-container-baby">
+              <img src="/images/baby_cat.jpg" alt="Baby Cat" className="image-baby-cat" />
+            </div>
+            <div className="button-back-container"> 
+              <button onClick={() => navigate('/')} className="button-back">Volver</button>
+            </div>
+            <div className="image-container-baby">
+              <img src="/images/baby_dog.jpg" alt="Baby Dog" className="image-baby-dog" />
+             </div>
+             
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
 }
 export default Login;

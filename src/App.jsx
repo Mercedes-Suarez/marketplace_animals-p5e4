b594b5981603c72cd32be991c9cd8fe1fa9350e8
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-03-04 09:47:21 +0100

    Fix some errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./views/Home/Home";
 import Management from "./views/Management/Management";
 import CatalogueView from "./views/CatalogueView/CatalogueView";
 import Product from "./views/Product/Product";
+import Login from "./components/Login/Login";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login /> } />
         <Route path="/catalogueView" element={<CatalogueView />} handle={{crumb : () => <span>catálogo</span>}}/>
         <Route path="/myProducts" element={<Management />} />
         <Route path="/products/:id" element={<Product />} handle={{crumb : () => <span>detalle de producto</span>}}/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { createRoot} from 'react-dom/client';
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'
 
@@ -12,6 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 
 
- // </React.StrictMode>,
- // document.getElementById('root'));
-
+  </React.StrictMode>,
+  
+)
